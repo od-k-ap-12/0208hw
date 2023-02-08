d68b1c330bfad004fde30afa2eb0792c09626285
Oleksandra <predictormagnolia@gmail.com>
2023-02-08 12:52:03 +0200

    Add project files.

---
+++ 0208hw/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0208hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(1, 2, 3);
+            Vector b = new Vector(1, 2, 3);
+            Vector c = new Vector();
+            double scalar = 3;
+            a.Print();
+            b.Print();
+            Console.WriteLine("Length: " + a.Length());
+            Console.WriteLine("Length: " + b.Length());
+            Console.WriteLine("Equal? "+a.AreEqual(b));
+            Console.WriteLine("a+b? " + a.VectorPlus(b));
+            Console.WriteLine("a-b? " + a.VectorMinus(b));
+            c = a.VectorMult(b);
+            Console.Write("a*b? ");
+            c.Print();
+            Console.Write("a-"+scalar+"? ");
+            a.ScalarMinus(scalar);
+            Console.Write("a+" + scalar + "? ");
+            a.ScalarPlus(scalar);
+
+        }
+    }
+}
---
+++ 0208hw/Vector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0208hw
+{
+    class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+        public Vector() : this(0,0,0){}
+        public Vector(double _x) : this(_x,0, 0){}
+        public Vector(double _x,double _y,double _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+        public void Input()
+        {
+            Console.WriteLine("x?");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y?");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("z?");
+            z = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("x: " + x + " y: " + y + " z: " + z);
+        }
+        public double X 
+        {
+            get => x;
+        }
+        public double Y
+        {
+            get => y;
+        }
+        public double Z 
+        {
+            get => z;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+        public void ScalarPlus(double scalar)
+        {
+            Console.WriteLine("x: " + (x + scalar) + " y: " + (y + scalar) + " z: " + (z + scalar));
+        }
+        public void ScalarMinus(double scalar)
+        {
+            Console.WriteLine("x: " + (x - scalar) + " y: " + (y - scalar) + " z: " + (z - scalar));
+        }
+        public bool AreEqual(Vector vector)
+        {
+            if (this.Length() == vector.Length())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double VectorPlus(Vector vector)
+        {
+            return ((this.x + vector.x) * (this.y + vector.y) * (this.z + vector.z));
+        }
+        public double VectorMinus(Vector vector)
+        {
+            return ((this.x - vector.x) * (this.y - vector.y) * (this.z - vector.z));
+        }
+        public Vector VectorMult(Vector vector)
+        {
+            Vector result = new Vector(this.x * vector.x, this.y * vector.y, this.z * vector.z);
+            return result;
+        }
+    }
+}
